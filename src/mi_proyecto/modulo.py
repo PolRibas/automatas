ab4f45611a8f7183eb2dbcb7ea3b15bb3f4522e1
PolRibas <polribasrovira@gmail.com>
2024-12-03 11:04:34 +0000

    Inicializar proyecto con estructura básica

--- src/mi_proyecto/modulo.py
+++ src/mi_proyecto/modulo.py
@@ -1,5 +1,7 @@
 def main():
     print("¡Hola, este es el módulo principal de mi proyecto!")
+    print("¡Espero que te guste!")
+    print("¡Hasta luego!")
 
 if __name__ == "__main__":
-    main()
+    main() 
